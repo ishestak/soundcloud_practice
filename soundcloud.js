81b44b20a222caafd085152dff1e6be827796305
ishestak <shestak.irina@gmail.com>
2014-09-12 13:49:55 -0700

    ahhh should have committed last night!

--- soundcloud.js
+++ soundcloud.js
@@ -1,23 +1,46 @@
-$(document).ready(function(){
 
-SC.initialize({
-  client_id: 'e1029c2d9a2079539d53193fe4cf5d69'
-});
 
-  $('form').on('submit', function(event){
-    event.preventDefault();
-    var query = $("#query").val();
+$(function(){
 
-    var trackUrl = 'http://api.soundcloud.com/tracks/'+query+'.json?client_id=e1029c2d9a2079539d53193fe4cf5d69';
-    $.ajax(trackUrl, {
-      success: function(response) {
-        var trackId = response.id
-        SC.get('/tracks/'+trackId, function(track){
-          SC.oEmbed(track.permalink_url,
-            $('#player'));
-        });
-      },
-    });
-
+  SC.initialize({
+    client_id: 'e1029c2d9a2079539d53193fe4cf5d69'
   });
+
+  var player = document.getElementById('player');
+
+    $('form').on('submit', function(event){
+      event.preventDefault();
+      var query = $("#query").val();
+
+      var userUrl = 'http://api.soundcloud.com/users/'+query+'.json?client_id=e1029c2d9a2079539d53193fe4cf5d69';
+      $.ajax(userUrl, {
+        success: function(response)
+        {
+          var location = response.city + ", " + response.country
+
+          var locUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address='+location+'&key=AIzaSyBYy-_E0FhtiolGMX9OO3WFWxQlPBk6Dfo';
+          $.ajax(locUrl, {
+            success: function(returns)
+            {c
+              var latlng = returns.results.geometry.bounds
+            },
+          });
+
+        },
+
+      });
+
+    });
 });
+
+function query(query, callback)
+{
+  $.ajax({url:query, success: callback})
+}
+
+query("somestuff", setMarkerOnMap)
+
+function setMarkerOnMap(location)
+{
+  var marker = new google.maps.marker(map, location: location)
+}
